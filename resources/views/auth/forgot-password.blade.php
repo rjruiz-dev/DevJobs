0120131b1a279fb69a57c5f929ebf685121537a1
rjr <rjruizsf@gmail.com>
2023-05-07 22:35:36 -0300

    Personalizando la vista de olvide mi password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('¿Olvidaste tu password? Coloca tu email de registro y enviaremos un enlace para que puedas crear uno nuevo.') }}
     </div>
 
     <!-- Session Status -->
@@ -16,10 +16,22 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+        <div class="flex justify-between my-5">           
+            <x-link
+                :href="route('login')"    
+            > 
+                Iniciar Sesión               
+            </x-link>
+
+            <x-link
+                :href="route('register')" 
+            > 
+                Crear Cuenta 
+            </x-link>           
         </div>
+
+        <x-primary-button class="w-full justify-center">
+            {{ __('Enviar Instrucciones') }}
+        </x-primary-button>
     </form>
 </x-guest-layout>

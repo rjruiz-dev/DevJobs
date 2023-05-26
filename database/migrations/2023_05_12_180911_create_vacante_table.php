c3fc0dfe75dd2624a084c5bc30cffba7ef93cbe8
rjr <rjruizsf@gmail.com>
2023-05-26 11:27:15 -0300

    Problema con la migracion opc 1

--- database/migrations/2023_05_12_180911_create_vacante_table.php
+++ database/migrations/2023_05_12_180911_create_vacante_table.php
@@ -13,8 +13,6 @@ return new class extends Migration
     {
         Schema::create('vacantes', function (Blueprint $table) {
             $table->id();
-            $table->string('titulo');
-            $table->foreignId('salario_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }

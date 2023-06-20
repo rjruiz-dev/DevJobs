1745a2a5890756a8babd9a85df0acdefbb84d7f6
rjruiz <rjruizsf@gmail.com>
2023-06-20 14:48:46 -0300

    Creando la ruta y pasando la informacion

--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -5,7 +5,7 @@
             <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">
                 {{-- <div class="leading-10">leading-10: incrementa el interlineado --}}
                 <div class="space-y-3">
-                    <a href="#" class="text-xl font-bold">
+                    <a href="{{ route('vacantes.show', $vacante->id ) }}" class="text-xl font-bold">
                         {{ $vacante->titulo }}
                     </a>
                     <p class="text-sm text-gray-600 font-bold">{{ $vacante->empresa }}</p>

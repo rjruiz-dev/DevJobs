7ba319e6d1506130a5adea89898fdc79dd40ecee
rjruiz <rjruizsf@gmail.com>
2023-07-02 09:37:11 -0300

    Validacion del fomrulario

--- app/Http/Livewire/PostularVacante.php
+++ app/Http/Livewire/PostularVacante.php
@@ -6,6 +6,28 @@ use Livewire\Component;
 
 class PostularVacante extends Component
 {
+    // public $cv; conectamos con postular-vacante.blade.php mediante wire:model="cv" 
+    public $cv;
+
+    // validacion
+    protected $rules = [
+        'cv' => 'required|mimes:pdf'
+    ];
+
+    // llamamos a la funcion declarada en el formulario en postular-vacante.blade.php
+    public function postularme() 
+    {
+        $this->validate();
+
+        // Alamacenar CV en el disco duro
+        
+        // Crear la vacante
+
+        // Crear notificacion y enviar el email
+
+        // Mosrtar el usuario un mensaje de ok
+    }
+
     public function render()
     {
         return view('livewire.postular-vacante');
--- resources/views/livewire/postular-vacante.blade.php
+++ resources/views/livewire/postular-vacante.blade.php
@@ -1,15 +1,23 @@
 <div class="bg-gray-100 p-5 mt-10 flex flex-col justify-center items-center">
     <h3 class="text-center text-2xl font-bold my-4">Postularme a esta vacante</h3>
-    <form class="w-96 mt-5">
+    <form wire:submit.prevent='postularme' class="w-96 mt-5">
         <div class="mb-4">
             <x-input-label for="cv" :value="__('Curriculumn o Hoja de Vida (PDF)')" />
             <x-text-input 
                 id="cv" 
                 type="file"
+                {{-- wire:model="cv" conectamos este atrib. con PostularVacant.php  --}}
+                wire:model="cv" 
                 accept=".pdf"
                 class="block mt-1 w-full"                
             />
         </div>
+
+        {{-- si exite error lo  mostramos --}}
+        @error('cv')
+            <livewire:mostrar-alerta :message="$message" />
+        @enderror
+
         <x-primary-button class="my-5">
             {{ __('Postularme') }}
         </x-primary-button>

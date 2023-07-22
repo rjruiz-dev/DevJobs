5867b8acfadd00fb3c27bf0e661c900f423cb319
rjruiz <rjruizsf@gmail.com>
2023-07-22 16:03:51 -0300

    Middleware: rol.reclutador

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/dashboard', [VacanteController::class, 'index'])->middleware(['auth', 'verified'])->name('vacantes.index');
 Route::get('/vacantes/create', [VacanteController::class, 'create'])->middleware(['auth', 'verified'])->name('vacantes.create');
@@ -26,7 +26,7 @@ Route::get('/vacantes/{vacante}edit', [VacanteController::class, 'edit'])->middl
 Route::get('/vacantes/{vacante}', [VacanteController::class, 'show'])->name('vacantes.show'); // no aplica autenticado ni verificado, todos pueden acceder
 
 // Notificaciones
-Route::get('/notificaciones', NotificacionController::class)->name('notificaciones');
+Route::get('/notificaciones', NotificacionController::class)->middleware(['auth', 'verified', 'rol.reclutador'])->name('notificaciones');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
